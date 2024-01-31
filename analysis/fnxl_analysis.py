2e160e51a78903111a8fc2270238ea8f414a00c2
Liamsalass <liamsalass@yahoo.com>
2024-01-31 18:07:20 -0500

    Refactor fnxl_analysis.py: Print label_count head and remove unnecessary code

--- analysis/fnxl_analysis.py
+++ analysis/fnxl_analysis.py
@@ -12,12 +12,10 @@ import argparse as ap
 def main(args):
     print ('Root directory: ', args.root)
     label_count = pd.read_csv(args.root + 'labelCount.csv')
-    print(label_count.head())
     print(label_count.shape)
-    # print max value in each column
-    print(label_count.max())
-    # print min value in each column
-    print(label_count.min())
+    print('Head:\n', label_count.head())
+  
+    
 
 
 
