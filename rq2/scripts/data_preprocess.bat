d1895d2adfbc6658c3e563cda645ef83d56b133d
LiamSalass <liamsalass@yahoo.com>
2024-04-19 21:41:19 -0400

    Refactor preprocessing script for FINER-139 dataset in rq2/finer_preprocess.py

--- rq2/scripts/data_preprocess.bat
+++ rq2/scripts/data_preprocess.bat
@@ -14,16 +14,16 @@ call conda activate xml
 
 
 echo "Preprocessing FINER train data"
-python finer_preprocess.py --output-dir "./processed" --split "train"  --vocab-size 50000 --max-len 728
+::python finer_preprocess.py --output-dir "./processed" --split "train"  --vocab-size 50000 --max-len 728
 
 echo "Preprocessing FINER test data"
-python finer_preprocess.py --output-dir "./processed" --split "test"  --vocab-size 50000 --max-len 728
+::python finer_preprocess.py --output-dir "./processed" --split "test"  --vocab-size 50000 --max-len 728
 
 echo "Converting train data to XML format"
-python preprocess.py --text-path "./processed_train/texts.npy" --tokenized-path "./processed_train/tokenized_texts.txt" --label-path "./processed_train/tags.npy" --vocab-path "./processed_train/vocab.npy" --emb-path "./processed_train/embeddings.npy" --w2v-model "./processed_train/word2vec_model.model" max-len 728
+python preprocess.py --text-path "./processed_train/texts.npy" --label-path "./processed_train/tags.npy" --vocab-path "./processed_train/vocab.npy" --max-len 500
 
 echo "Converting test data to XML format"
-python preprocess.py --text-path "./processed_test/texts.npy" --tokenized-path "./processed_test/tokenized_texts.txt" --label-path "./processed_test/tags.npy" --vocab-path "./processed_test/vocab.npy" --emb-path "./processed_test/embeddings.npy" --w2v-model "./processed_test/word2vec_model.model" max-len 728
+python preprocess.py --text-path "./processed_test/texts.npy" --label-path "./processed_test/tags.npy" --vocab-path "./processed_test/vocab.npy"  --max-len 500
 
 echo "All data preprocessed and ready for XML model training."
 
